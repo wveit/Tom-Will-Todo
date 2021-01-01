bbef13cf7280008b098e73c0e2b1592caf02e60c
Wilbert Veit <wilbertveit@rocketmail.com>
2020-12-31 21:23:14 -0800

    Registration checks if email in use

--- backend/routers/users.js
+++ backend/routers/users.js
@@ -3,22 +3,47 @@ const router = express.Router();
 const User = require('../models/User');
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
+const { body, header, validationResult } = require('express-validator');
 
 const jwtSecret = 'this is my jwt secret';
 
 // registration endpoint
-router.post('/users', async function (req, res) {
-    const { name, email, password } = req.body;
-    const hashedPassword = await bcrypt.hash(password, 8);
-    const newUser = await User.create({
-        name,
-        email,
-        hashedPassword,
-    });
-    const id = newUser.id;
-    const token = jwt.sign({ id }, jwtSecret);
-    res.json({ token });
-});
+router.post(
+    '/users',
+    body('name').isLength({ min: 2 }),
+    body('email').isEmail(),
+    body('password').isLength({ min: 6 }),
+    async function (req, res) {
+        // handle validation results
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            const errorObj = {};
+            errors.array().forEach(function (error) {
+                errorObj[error.param] = error.msg;
+            });
+            return res.status(400).json({ errors: errorObj });
+        }
+
+        const { name, email, password } = req.body;
+        const existingUser = await User.findOne({ email });
+        if (existingUser) {
+            return res
+                .status(400)
+                .json({ errors: { email: 'Email already in use' } });
+        }
+
+        // the actual registration code
+        const hashedPassword = await bcrypt.hash(password, 8);
+        const newUser = await User.create({
+            name,
+            email,
+            hashedPassword,
+        });
+        const id = newUser.id;
+        const token = jwt.sign({ id }, jwtSecret);
+        res.json({ token });
+    }
+);
 
 // get current user
 router.get('/users/me', function (req, res) {
